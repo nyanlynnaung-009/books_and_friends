67dd6ac9c33c9a3e715ddee12b69ddb129001768
nyanlynn <mrnyanlynn.369@gmail.com>
2026-03-14 11:40:43 +0630

    feat(pwa): Add maskable icons and ID to PWA manifest
    Enhances the Progressive Web App configuration by adding a unique ID to the manifest and specifying maskable icons. This improves the PWA's integration with various platforms and app launchers.

--- vite.config.ts
+++ vite.config.ts
@@ -25,16 +25,19 @@ export default defineConfig(({mode}) => {
           background_color: '#ffffff',
           display: 'standalone',
           start_url: '/',
+          id: '/',
           icons: [
             {
               src: 'icon-192x192.png',
               sizes: '192x192',
-              type: 'image/png'
+              type: 'image/png',
+              purpose: 'any maskable'
             },
             {
               src: 'icon-512x512.png',
               sizes: '512x512',
-              type: 'image/png'
+              type: 'image/png',
+              purpose: 'any maskable'
             }
           ]
         }
